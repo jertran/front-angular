ced107bb4d2aeb78bd2a350e86d95c78ca5c6b37
jeremy.tran <jeremy.tran@utbm.fr>
2018-03-24 13:31:41 -0400

    edit imports

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MeteoComponent } from './meteo/meteo.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MeteoComponent
       ],
     }).compileComponents();
   }));
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MeteoComponent } from './meteo/meteo.component';
 
 @Component({
   selector: 'app-root',
