b52252a39b0e895f6a8e5cb9e595014dc1834dd8
jeremy.tran <jeremy.tran@utbm.fr>
2018-03-24 13:11:19 -0400

    edit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MeteoComponent } from './meteo/meteo.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
