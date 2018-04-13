364544082950e99dc59039433c0c910179374b06
jeremy.tran <jeremy.tran@utbm.fr>
2018-04-13 16:08:43 -0400

    add modules to weather test spec

--- src/app/weather.service.spec.ts
+++ src/app/weather.service.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { WeatherService } from './weather.service';
 
 describe('WeatherService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
       providers: [WeatherService]
     });
   });
