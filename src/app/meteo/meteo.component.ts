04327006321871c1ed79ed0958477fc8cb75389e
jeremy.tran <jeremy.tran@utbm.fr>
2018-04-20 19:18:35 -0400

    Adding getter to get objects using Ruby app JSON api

--- src/app/meteo/meteo.component.ts
+++ src/app/meteo/meteo.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
 import { Town } from '../town';
 
 @Component({
   selector: 'app-meteo',
   templateUrl: './meteo.component.html',
-  styleUrls: ['./meteo.component.css']
+  styleUrls: ['./meteo.component.css'],
+  providers: [WeatherService]
 })
 export class MeteoComponent implements OnInit {
   
-  town: Town = {id: 1, name: 'VALLATOWN', temperature: 1000, icon: "sun"};
+  town = new Town();
   
-  constructor() { }
+  constructor(private WeatherService: WeatherService) { }
 
   ngOnInit() {
+    this.WeatherService.getTownById(1).subscribe(town => this.town = town);
   }
 
 }
